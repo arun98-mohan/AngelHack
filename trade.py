4abb89f43106dcd3e73c83f254a39a8ae01ab854
Arun Prasad <arun98.mohan@gmail.com>
2019-04-27 18:53:08 +0530

    Python Scripts
    for trading

---
+++ trade.py
@@ -0,0 +1,51 @@
+#Prototyping with Five Farms and each farm considered to be one single share
+#For Development Purpose - One Farm = One Share
+#Stocks file format - stock_id,ltp
+
+import csv
+
+wish_list={}
+
+def read_Share_values():
+#Reading the Share Values
+    with open("share_values.csv","r") as f:
+        print("Share Values")
+        values=csv.reader(f)
+        print("Share ID , LTP")
+        for i in f:
+            print(i)
+
+#When a person is interested in selling the stock, he adds his stock
+#to the wish list
+
+def add_Stock_to_wish_list(id,price):
+    wish_list[id]=price
+
+#Buy a stock
+#buy when wish list has a stock of same ID and value less than or equal to requested amount
+
+def buy_Stock(id,price):
+    for key,value in wish_list:
+        if(id==key and value<=price):
+            #code for buying the stock
+            #add to block for blockchain
+
+            del wish_list[id]
+            return 0
+        return 1
+
+
+choice=input("Buy or Sell [Enter buy || sell] or see for Current Value ")
+if(choice=="buy"):
+    id=input("Enter the Stock ID: ")
+    value=input("Enter the Value Desired: ")
+    temp=buy_Stock(id,value)
+    if(temp==1):
+        print("Sorry Enter a better Amount")
+elif(choice=="sell"):
+    id=input("Enter the Stock ID: ")
+    value=input("Enter the Minimum amount to be sold for: ")
+    add_Stock_to_wish_list(id,value)
+elif(choice=="see"):
+    read_Share_values()
+
